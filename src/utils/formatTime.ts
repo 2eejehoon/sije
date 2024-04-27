c8cde329464248d8234f3a8cf89b90c97423dd9f
이제훈 <2eejehoon@gmail.com>
2024-04-27 18:18:00 +0900

    formatTime 유틸 함수

---
+++ src/utils/formatTime.ts
@@ -0,0 +1,21 @@
+export const formatTime = (date: Date) => {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  const formatHours = hours < 10 ? "0" + hours : hours;
+  const formatMinutes = minutes < 10 ? "0" + minutes : minutes;
+
+  return formatHours + ":" + formatMinutes;
+};
+
+export const formatTimeWithSeconds = (date: Date) => {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  const formatHours = hours < 10 ? "0" + hours : hours;
+  const formatMinutes = minutes < 10 ? "0" + minutes : minutes;
+  const formatSeconds = seconds < 10 ? "0" + minutes : minutes;
+
+  return formatHours + ":" + formatMinutes + ":" + formatSeconds;
+};
