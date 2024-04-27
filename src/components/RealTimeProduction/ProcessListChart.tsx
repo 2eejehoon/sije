581ea50bf2d4c0989cb9c374dc644a4e15f7f80a
이제훈 <2eejehoon@gmail.com>
2024-04-27 11:40:27 +0900

    ProcessListChart 수정

--- src/components/RealTimeProduction/ProcessListChart.tsx
+++ src/components/RealTimeProduction/ProcessListChart.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import processListObj from "../../../public/data/process-list.json";
-import { Bar, XAxis, YAxis, ResponsiveContainer, ComposedChart, Legend } from "recharts";
+import { Bar, XAxis, YAxis, ResponsiveContainer, ComposedChart, Legend, Rectangle } from "recharts";
+import { useState } from "react";
 
 const data = processListObj.processList.map((process) => {
   return {
@@ -10,21 +11,45 @@ const data = processListObj.processList.map((process) => {
 });
 
 const ProcessListChart = () => {
+  const [activeIndex, setActiveIndex] = useState(-1);
+
+  const handleMouseEnter = (_: unknown, index: number) => {
+    setActiveIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setActiveIndex(-1);
+  };
+
   return (
     <Wrapper>
       <ResponsiveContainer width={"100%"} height={"100%"}>
         <ComposedChart data={data} margin={{ top: 50 }}>
           <XAxis dataKey="processName" />
-          <YAxis />
+          <YAxis label={{ value: "(pcs)", angle: 0, position: "insideTopLeft" }} />
           <Legend wrapperStyle={{ top: 0 }} />
-          <Bar isAnimationActive={false} stackId={"id"} dataKey="output" fill="#BDBFC2" barSize={14} />
           <Bar
             isAnimationActive={false}
-            stackId={"id"}
+            stackId="id"
+            dataKey="output"
+            fill="#BDBFC2"
+            barSize={14}
+            activeIndex={activeIndex}
+            onMouseEnter={handleMouseEnter}
+            activeBar={<Rectangle isAnimationActive={false} fill="#FF9B3F" onMouseLeave={handleMouseLeave} />}
+          />
+          <Bar
+            isAnimationActive={false}
+            stackId="id"
             dataKey="expectOutput"
-            fill="#5550FF"
+            fill="transparent"
             barSize={14}
             radius={[12, 12, 0, 0]}
+            stroke="#5550FF"
+            strokeDasharray="5 5"
+            activeIndex={activeIndex}
+            onMouseEnter={handleMouseEnter}
+            activeBar={<Rectangle isAnimationActive={false} fill="#5550FF" onMouseLeave={handleMouseLeave} />}
           />
         </ComposedChart>
       </ResponsiveContainer>
