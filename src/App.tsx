9d6d9b1092b13981f951488b560992adccac38ca
이제훈 <2eejehoon@gmail.com>
2024-04-27 14:19:05 +0900

    BrowserRouter로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RealTimeProductionPage from "./pages/RealTimeProductionPage";
+import LineManagementPage from "./pages/LineManagementPage";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/line-management" element={<LineManagementPage />} />
+        <Route path="/real-time-production" element={<RealTimeProductionPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
